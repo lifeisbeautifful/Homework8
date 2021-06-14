a391d3a68db38e190f18be0f2996af8680f809c2
qwerty-commits <Oksankagnyp@gmail.com>
2021-06-14 13:32:34 +0300

    H8

---
+++ Task1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkLesson8
+{
+    /*Используя Visual Studio, создайте проект по шаблону Console Application.
+Создайте программу, в которой создайте класс хвост, который содержит в себе поля длину хвоста типа int и вид хвоста типа string, 
+    создать свойства доступа и конструктор пользовательский класса.Создать класс хвостатое животное, содержащий хвост, цвет(строка), 
+    возраст.Определить public - производный класс собака, имеющий дополнительный параметр-кличку(строка). 
+В классе собака создать метод для отображения полной информации о собаке.*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog("Red",5,"Marta");
+            dog.Info();
+            Console.ReadLine();
+        }
+    }
+
+    public class Tail
+    {
+        private int tailLength;
+        private string tailType;
+        public int TailLength 
+        {
+            get { return tailLength; }
+            set { tailLength = value; }
+        }
+        public string TailType 
+        {
+            get { return tailType; }
+            set { tailType = value; }
+        }
+        public Tail(int length,string type)
+        {
+            TailLength = length;
+            TailType = type;
+        }
+    }
+
+    public class TailedAnimal
+    {
+        public Tail tail = new Tail(40, "fluffy");
+        private string color;
+        private int age;
+        public string Color 
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        public int Age 
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+    public class Dog : TailedAnimal
+    {
+        private string name;
+        public string Name 
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public Dog(string color, int age, string name)
+        {
+            Color = color;
+            Name = name;
+            Age = age;
+        }
+        public void Info()
+        {
+            Console.WriteLine("Name: "+Name);
+            Console.WriteLine("Age: "+Age);
+            Console.WriteLine("Color: "+Color);
+            Console.WriteLine("Tail length: "+tail.TailLength);
+            Console.WriteLine("Tail type: "+tail.TailType);
+        }
+    }
+}
